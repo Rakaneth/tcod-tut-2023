68f46b83eb069ad93ba2fcbeef0d295ba8fffce1
Rakaneth <rakaneth@gmail.com>
2023-07-26 06:56:46 -0400

    happy ruff

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,4 +1,4 @@
-python -m venv env
-call .\env\Scripts\activate
+python -m venv .venv
+call .\.venv\Scripts\activate
 python -m pip install --upgrade pip
 pip install -r requirements.txt
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-python3 -m venv env
-. ./env/bin/activate
+python3 -m venv .venv
+. ./.venv/bin/activate
 python -m pip install --upgrade pip
 pip install -r requirements.txt
